b70e9d64c65d8b9dbd1237180bec42fe0d8dd341
ADI9325 <adityabagade04@gmail.com>
2024-12-03 21:52:11 +0530

    C-18

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,7 @@ use App\Repositories\RegistrationStudentRepository;
 use App\Repositories\AlumniSpeakRepository;
 use App\Repositories\PlacedStudentRepository;
 use App\Repositories\AnnouncementRepository;
+use App\Repositories\QuestionRepository;
 
 //Interfaces imports
 use App\Repositories\Interfaces\EnquiryRepositoryInterface;
@@ -21,7 +22,7 @@ use App\Repositories\Interfaces\StudentTestimonialRepositoryInterface;
 use App\Repositories\Interfaces\AlumniSpeakRepositoryInterface;
 use App\Repositories\Interfaces\PlacedStudentRepositoryInterface;
 use App\Repositories\Interfaces\AnnouncementRepositoryInterface;
-
+use App\Repositories\Interfaces\QuestionRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -39,6 +40,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AlumniSpeakRepositoryInterface::class, AlumniSpeakRepository::class);
         $this->app->bind(PlacedStudentRepositoryInterface::class, PlacedStudentRepository::class);
         $this->app->bind(AnnouncementRepositoryInterface::class, AnnouncementRepository::class);
+        $this->app->bind(QuestionRepositoryInterface::class, QuestionRepository::class);
     }
 
     /**
